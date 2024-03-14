d3613d01298bcdc164815754df1b4ccb3197851f
Tzur Paldi <tzur.paldi@outlook.com>
2024-03-14 11:37:02 +0200

    initial

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import base, { expect } from '@playwright/test';
+import {test as base, expect } from '@playwright/test';
 import type { PerformanceOptions, PlaywrightPerformance, PerformanceWorker } from "../src/performance-fixture";
 import { playwrightPerformance } from "../src/performance-fixture";
 
